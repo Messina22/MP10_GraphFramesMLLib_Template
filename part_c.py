9d7f383355b99ae4d6fdfeab3f5f2ce93b972b67
Nick Messina <nicknmessina@gmail.com>
2024-04-14 17:54:13 -0500

    Part c done

--- part_c.py
+++ part_c.py
@@ -3,7 +3,7 @@ from pyspark.sql import SparkSession
 from graphframes import *
 
 sc = SparkContext()
-spark = SparkSession.builder.appName('fun').getOrCreate()
+spark = SparkSession.builder.appName("fun").getOrCreate()
 
 
 def get_shortest_distances(graphframe, dst_id):
@@ -11,27 +11,30 @@ def get_shortest_distances(graphframe, dst_id):
     # Find shortest distances in the given graphframe to the vertex which has id `dst_id`
     # The result is a dictionary where key is a vertex id and the corresponding value is
     # the distance of this node to vertex `dst_id`.
-    return {}
+    results = graphframe.shortestPaths(landmarks=[dst_id])
+    distances = results.rdd.map(
+        lambda row: (row.id, row.distances.get(dst_id, -1))
+    ).collectAsMap()
+    return distances
 
 
 if __name__ == "__main__":
     vertex_list = []
     edge_list = []
-    with open('dataset/graph.data') as f:
+    with open("dataset/graph.data") as f:
         for line in f:
-            # TODO: Parse line to get vertex id
-            src = None
-            # TODO: Parse line to get ids of vertices that src is connected to
-            dst_list = []
+            split_line = line.split()
+            src = split_line[0]
+            dst_list = split_line[1:]
             vertex_list.append((src,))
             edge_list += [(src, dst) for dst in dst_list]
 
-    vertices = spark.createDataFrame(...)  # TODO: Create dataframe for vertices
-    edges = spark.createDataFrame(...)  # TODO: Create dataframe for edges
+    vertices = spark.createDataFrame(vertex_list, ["id"])
+    edges = spark.createDataFrame(edge_list, ["src", "dst"])
 
     g = GraphFrame(vertices, edges)
     sc.setCheckpointDir("/tmp/shortest-paths")
 
     # We want the shortest distance from every vertex to vertex 1
-    for k, v in get_shortest_distances(g, '1').items():
+    for k, v in get_shortest_distances(g, "1").items():
         print(k, v)
